9ee4e43ab72225670dacde805190371d67bc96f5
vinitmule32 <vinitmule32@.com>
2026-03-11 19:48:10 +0530

    Made changes in backend - update cluster

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,26 @@
 import mongoose from "mongoose";
 
-export const  connectDB = async () =>{
+export const connectDB = async () => {
+  //   await mongoose
+  //     .connect
+  //     await mongoose.connect('mongodb+srv://vinitmule:vinitmule123@cluster0.4c4qa.mongodb.net/food-del').then(()=>{
+  //     console.log("DB Connected")
+  //     }
 
-    await mongoose.connect('mongodb+srv://vinitmule:vinitmule123@cluster0.4c4qa.mongodb.net/food-del').then(()=>console.log("DB Connected"));
-//    mongodb+srv://vinitmule:<db_password>@cluster0.4c4qa.mongodb.net/
+  //     .then(() => {
+  //         console.log("DB connected");
+  //     })
+  //     .catch((err) => {
+  //         console.log(err);
+  //     });
 
-}
+  await mongoose
+    .connect(
+      "mongodb+srv://vinitmule:mulevinit32@cluster0.4c4qa.mongodb.net/food-del",
+    )
+    .then(() => {
+      console.log("DB Connected");
+    });
+};
 
-
-// add your mongoDB connection string above.
-// Do not use '@' symbol in your databse user's password else it will show an error.
+// "mongodb+srv://vinitmule:mulevinit32@cluster0.4c4qa.mongodb.net/food-del?retryWrites=true&w=majority"

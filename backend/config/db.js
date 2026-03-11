b87d3beb75fe0c51f9accee68b845641709d288f
vinitmule32 <vinitmule32@.com>
2026-03-11 20:04:33 +0530

    simple checkup in app

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,18 +1,6 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-  //   await mongoose
-  //     .connect
-  //     await mongoose.connect('mongodb+srv://vinitmule:vinitmule123@cluster0.4c4qa.mongodb.net/food-del').then(()=>{
-  //     console.log("DB Connected")
-  //     }
-
-  //     .then(() => {
-  //         console.log("DB connected");
-  //     })
-  //     .catch((err) => {
-  //         console.log(err);
-  //     });
 
   await mongoose
     .connect(

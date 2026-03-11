6168860336dd0e64794c0422421f49c073a81919
vinitmule32 <vinitmule32@.com>
2026-03-11 22:56:46 +0530

    updated provider.tf file

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,6 @@
 provider "aws" {
-  region = "ap-south-1"
+  region                      = "ap-south-1"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
 }
